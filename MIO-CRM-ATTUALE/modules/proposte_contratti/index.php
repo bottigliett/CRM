2288fed7a0ea50f825e40f65140cc83ad1ff680c
justin <marangonidavide05@gmail.com>
2025-12-30 22:38:25 +0100

    Remove MIO-CRM-ATTUALE folder from repository and add to gitignore

--- MIO-CRM-ATTUALE/modules/proposte_contratti/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Directory protected
-exit;
-?>
--- MIO-CRM-ATTUALE/modules/task_manager/ajax/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// File: /modules/fatture/ajax/index.php
-// File di sicurezza per impedire accesso diretto alla directory AJAX
-
-// Redirect alla pagina principale del modulo
-header('Location: /modules/task_manager/');
-exit;
-?>
